d57e4d0e23715ecdc0581a2cf59e0ea3aa35bbb7
fitripuspitasari <71800875+fitripuspitasari@users.noreply.github.com>
2020-11-19 09:19:57 +0700

    Add files via upload

---
+++ mid/teori/inheritance/main.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+class main{
+	public static void main(String[]args){
+		Proses p = new Proses();
+		
+		p.input();
+		p.pilihan();
+		p.pembayaran();
+		p.cetak();
+		
+		
+	}
+}
